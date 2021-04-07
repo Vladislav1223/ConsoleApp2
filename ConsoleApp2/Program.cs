3830c56b910fafff58653c601154e82d6172d6c5
Vladislav <vlad.king.20012109@gmail.com>
2021-04-07 23:17:07 +0300

    zmina3

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,6 +7,9 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
